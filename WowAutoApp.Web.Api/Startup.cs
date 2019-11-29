148714d4494058fbd9402c59081eaa0ffea6e747
Stepan Sturko <sturko31@gmail.com>
2019-11-29 02:19:10 +0200

    BE - Configure project and architect

---
+++ WowAutoApp.Web.Api/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace wowautoapp
+{
+    /// <summary>
+    /// Startup project
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Configuration startup project
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Inject project configuration
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add mvc
+            services.AddMvc()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime.Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="applicationBuilder"></param>
+        /// <param name="hostingEnvironment"></param>
+        /// <param name="loggerFactory"></param>
+        /// <param name="services"></param>
+        public void Configure(IApplicationBuilder applicationBuilder,
+            IHostingEnvironment hostingEnvironment,
+            ILoggerFactory loggerFactory,
+            IServiceProvider services)
+        {
+            if (hostingEnvironment.IsDevelopment())
+                applicationBuilder.UseDeveloperExceptionPage();
+
+            applicationBuilder.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
