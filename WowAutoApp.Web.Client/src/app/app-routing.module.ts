188d6f2ae4f20499700197dd61c84fc70286713d
Acer <Acer@COMPUTER>
2019-12-01 19:03:14 +0200

    create home component

--- WowAutoApp.Web.Client/src/app/app-routing.module.ts
+++ WowAutoApp.Web.Client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: 'register', loadChildren: './components/registration/registration.module#RegistrationModule' }
+  { path: '', component: HomeComponent },
+  { path: '', loadChildren: './components/registration/registration.module#RegistrationModule' }
 ];
 
 @NgModule({
--- WowAutoApp.Web.Client/src/app/app.module.ts
+++ WowAutoApp.Web.Client/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
