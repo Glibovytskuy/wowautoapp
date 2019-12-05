c76422f0372b0475c226ac1d5fda8c1d79a1ab33
Vladyslav S <sanahurskyi.v@gmail.com>
2019-12-05 10:43:37 +0200

    credit-application first changes

--- WowAutoApp.Web.Client/src/app/app-routing.module.ts
+++ WowAutoApp.Web.Client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CreditApplicationComponent } from './components/credit-application/credit-application.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '', loadChildren: './components/registration/registration.module#RegistrationModule' }
+  { path: '', loadChildren: './components/registration/registration.module#RegistrationModule' },
+  { path: 'credit-application', component: CreditApplicationComponent }
 ];
 
 @NgModule({
