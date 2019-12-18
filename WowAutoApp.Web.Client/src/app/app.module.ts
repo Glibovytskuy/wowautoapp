4c5c8eeaffde4fc8a5a85cf8d03186cefd853bda
Vladyslav S <sanahurskyi.v@gmail.com>
2019-12-18 17:08:26 +0200

    fix package.json, add ngx-bootstrap

--- WowAutoApp.Web.Client/src/app/app.module.ts
+++ WowAutoApp.Web.Client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './components/shared/shared.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +25,8 @@ import { HttpClientService } from './core/general-services/http-client.service';
     FormsModule,
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
