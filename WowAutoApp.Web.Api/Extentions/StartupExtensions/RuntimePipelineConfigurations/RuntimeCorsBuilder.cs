f8a8e7ff8b91e46024defd9e4cab55b5ef0ea7b7
Stepan Sturko <sturko31@gmail.com>
2019-12-25 03:45:16 +0200

    BE - Fixed CORS

---
+++ WowAutoApp.Web.Api/Extentions/StartupExtensions/RuntimePipelineConfigurations/RuntimeCorsBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace wowautoapp.Extensions.StartupExtensions.RuntimePipelineConfigurations
+{
+    /// <summary>
+    /// Runtime configuration builder for logger
+    /// </summary>
+    public static partial class RuntimeConfigurationBuilder
+    {
+        /// <summary>
+        /// Uet specified CORS policy
+        /// </summary>
+        /// <param name="applicationBuilder"></param>
+        /// <param name="configuration"></param>
+        public static void UseRuntimeCorsBuilder(this IApplicationBuilder applicationBuilder, IConfiguration configuration)
+        {
+            var policyName = configuration.GetValue<string>("CorsPolicyName");
+            applicationBuilder.UseCors(policyName);
+        }
+    }
+}
