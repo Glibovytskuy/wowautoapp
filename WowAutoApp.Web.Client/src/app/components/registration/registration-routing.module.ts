aa29ca136faede38510c28fde2cd1cf405b637d5
Bohdan Gubernat <gubernat97bogdan@gmail.com>
2020-01-29 13:06:43 +0200

    Add regisration modal with validation

--- WowAutoApp.Web.Client/src/app/components/registration/registration-routing.module.ts
+++ WowAutoApp.Web.Client/src/app/components/registration/registration-routing.module.ts
@@ -4,14 +4,16 @@ import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { ChooseApplicationComponent } from './choose-application/choose-application.component';
-import { CreditApplicationComponent } from './credit-application/credit-application.component';
+import { RegisterComponent } from './register/register.component';
+
+
 
 const routes: Routes = [
   { path: 'recover-password', component: RecoverPasswordComponent },
   { path: 'change-password', component: ChangePasswordComponent },
   { path: 'login', component: LoginComponent },
   { path: 'applications', component: ChooseApplicationComponent },
-  { path: 'applications/personal', component: CreditApplicationComponent }
+  { path: 'registration', component: RegisterComponent }
 ];
 
 @NgModule({
